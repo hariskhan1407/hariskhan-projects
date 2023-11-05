06aaae79aadfc7de80d7841017b430b751df1d6e
Haris Khan <148951890+hariskhan1407@users.noreply.github.com>
2023-11-05 08:30:19 +0000

    Today I learned about variables in JS

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,19 @@
+var name = 'Haris Khan';
+let accountID = 222444555
+const accountPassward = 333555666
+city = 'allahabad'
+let rollno;
+
+name = 'Kaif Khan';
+accountID = 333444555
+// accountPassward = 444555666 not allowed
+city = 'manali'
+
+console.table([name,accountID,accountPassward,city,rollno]);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
