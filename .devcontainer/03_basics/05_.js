ff120b2efbe93dc0fe7b24b5a2252eb9b18affdf
Haris Khan <148951890+hariskhan1407@users.noreply.github.com>
2024-01-15 21:58:51 +0530

    new file uploaded

---
+++ .devcontainer/03_basics/05_.js
@@ -0,0 +1,13 @@
+//Execution context
+//1. Global Execution context
+//2. Functional Execution context
+//3. Eval Execution context
+
+//Memory Creation Phase(memory allocate)
+//execution phase(+-*/)
+
+function addNum(num1,num2){
+    let total = num1 + num2
+    return total
+}
+console.log(addNum(3,5))
